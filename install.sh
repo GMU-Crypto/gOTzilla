ab938c4b6fd3a02ea0c6ec34103312a41e1c987f
Panos Chatzigiannis <bomberb17@gmail.com>
2022-06-10 13:07:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ git clone --branch 3.2.0 https://github.com/Microsoft/SEAL/ && cd ~/SEAL/native/
 git clone https://github.com/microsoft/SealPIR && cd SealPIR && git reset --hard ccf86c50fd3291d7d720f1b9547022ebf3c9b6b0 && cmake . && make -j8 && cd ~
 git clone https://github.com/data61/MP-SPDZ && cd MP-SPDZ && make -j8 tldr && make -j8 && cd ~
 git clone https://github.com/emp-toolkit/emp-tool && cd emp-tool && cmake . && make -j8 && sudo make install && cd ~
-cd ~/gotzilla && cmake . && make -j8
+cd ~/gOTzilla && cmake . && make -j8
